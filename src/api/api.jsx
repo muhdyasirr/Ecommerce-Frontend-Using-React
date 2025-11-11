e3a9131e45b6ef09004412db5316b8c68ecc1b34
muhdyasirr <muhdyasir223@gmail.com>
2025-11-11 16:30:52 +0530

    refactor

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios, { Axios } from "axios"
 
 const api=axios.create({
-    baseURL:"http://40.192.14.44",
+    baseURL:"http://40.192.14.44/api",
      withCredentials:true,
 })
 export default api
